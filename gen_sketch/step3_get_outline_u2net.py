d30acb92edf664d846cc0966cf660e4034c45f76
吴昊展 <48346197+WHZMM@users.noreply.github.com>
2024-04-29 15:37:22 +0800

    Add files via upload

---
+++ gen_sketch/step3_get_outline_u2net.py
@@ -0,0 +1,10 @@
+# UTF-8
+
+# This is the 3-rd step
+
+# We need to extract outline information from face images.
+
+# We used the open source code and pre-trained model of the paper "U2-Net: Going Deeper with Nested U-Structure for Salient Object Detection".
+# The web link is: https://github.com/xuebinqin/U-2-Net
+# They provide a pre-trained model that extract face images outline: u2net_portrait.pth
+# Store the outline images in the folder "outline"
