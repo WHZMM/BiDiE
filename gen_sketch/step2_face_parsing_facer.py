5666491f24be1fe9d1079ad4ce56de46894268b5
吴昊展 <48346197+WHZMM@users.noreply.github.com>
2024-04-30 22:25:49 +0800

    Update step2_face_parsing_facer.py

--- gen_sketch/step2_face_parsing_facer.py
+++ gen_sketch/step2_face_parsing_facer.py
@@ -8,6 +8,6 @@
 # We use Facer (https://github.com/FacePerceiver/facer) for face-parsing.
 # The face image is divided into 11 types of areas, and their grayscale values are i*25 (i=0,1,2, ... ,10)
 # For the values of 11 types of areas, please refer to the sample images in: BiDiE/gen_sketch/face_parsing/
-# Store the face-parsing images in the folder "face_parsing"
+# Save the face-parsing images in the folder "face_parsing"
 
 # You can also use other models to obtain face_parsing results.
